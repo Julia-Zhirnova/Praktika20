78d0a87e0d84a3ce08c2a4fd535540c86f99fbf4
Julia-Zhirnova <52165649+Julia-Zhirnova@users.noreply.github.com>
2020-07-14 14:50:29 +0300

    Update __main__.py

--- REST API/prog/__main__.py
+++ REST API/prog/__main__.py
@@ -1,3 +1,10 @@
 from .web_api import WebApp
 
-WebApp().start()
+from prog.database import DataBaseConnector, DataBaseConfig
+
+database_config = DataBaseConfig()
+db = DataBaseConnector()
+database_config.database_address = 'influx'
+db.set_configuration(database_config)
+db.connect()
+WebApp(db).start()
