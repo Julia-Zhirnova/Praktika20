b4fe11eec8c973265de92b1c6c191bff8954acd5
Julia-Zhirnova <52165649+Julia-Zhirnova@users.noreply.github.com>
2020-07-22 02:03:20 +0300

    Update msqtt.py

--- MQTT/prog/msqtt.py
+++ MQTT/prog/msqtt.py
@@ -1,42 +1,22 @@
-from multiprocessing import Process
-
 import paho.mqtt.client as mqtt
-from influxdb import InfluxDBClient
-import prog.prog_constants as CNST
+from .publisher import PublisherClass, Var
+
+# https://pypi.org/project/paho-mqtt/
+MQTT_ADDR = 'mosquitto'
+MQTT_PORT = 1883
 
 
-class MQTTSetter:
-    def __init__(self, host_influx, host_mqtt , var, topic):
-        self._var = var
-        self._topic = topic
-        self._db_class = InfluxDBClient(host_influx, port=8086, database=CNST.INFLUXDB_DB_NAME)
+def listener_func(pub: PublisherClass, name: str, sub: str):
 
-        self._cli = mqtt.Client()
+    def on_connect(client, userdata, flags, rc):
+        client.subscribe(sub + '/' + name)
 
-        def on_connect(client, userdata, flags, rc):
-            client.subscribe(self._topic + "/" + self._var)
+    def on_message(client, userdata, msg):
+        a = Var(float(msg.payload.decode()), 0, False)
+        pub.add_field(name, a)
 
-        def on_message(client, userdata, msg):
-            self._db_class.write_points([
-                    {
-                        "measurement": CNST.INFLUXDB_MEAS_NAME,
-                        "fields": {
-                            self._var: float(msg.payload.decode('ascii'))
-                        }
-                    }
-                ])
-
-        self._cli.on_connect = on_connect
-        self._cli.on_message = on_message
-        self._cli.connect(host_mqtt, 1883, 60)
-        self._cli.loop_forever()
-
-
-def MqTTServerStart(host_influx, host_mqtt , var, topic):
-    MQTTSetter(host_influx, host_mqtt, var, topic)
-
-
-def StartMqtt(host_influx, host_mqtt , var, topic):
-    pr = Process(target=MqTTServerStart, args=(host_influx, host_mqtt , var, topic))
-    pr.start()
-    return pr
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(MQTT_ADDR, MQTT_PORT)
+    client.loop_forever()
